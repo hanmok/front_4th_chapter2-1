ac26f57fcf99f10a615009d9e6d8f2b5b2428f85
hanmok <dmammmm@naver.com>
2025-01-06 03:38:26 +0900

    refactor: improve html text visibility

--- src/main.original.js
+++ src/main.original.js
@@ -235,21 +235,13 @@ addBtn.addEventListener('click', () => {
       const newItem = document.createElement('div');
       newItem.id = itemToAdd.id;
       newItem.className = 'flex justify-between items-center mb-2';
-      newItem.innerHTML =
-        '<span>' +
-        itemToAdd.name +
-        ' - ' +
-        itemToAdd.price +
-        '원 x 1</span><div>' +
-        '<button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="' +
-        itemToAdd.id +
-        '" data-change="-1">-</button>' +
-        '<button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="' +
-        itemToAdd.id +
-        '" data-change="1">+</button>' +
-        '<button class="remove-item bg-red-500 text-white px-2 py-1 rounded" data-product-id="' +
-        itemToAdd.id +
-        '">삭제</button></div>';
+      newItem.innerHTML = `
+      <span>${itemToAdd.name} - ${itemToAdd.price}원 x 1</span> 
+      <div> 
+        <button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="${itemToAdd.id}  data-change="-1">-</button> 
+        <button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="${itemToAdd.id}" data-change="1">+</button> 
+        <button class="remove-item bg-red-500 text-white px-2 py-1 rounded" data-product-id="${itemToAdd.id}">삭제</button> 
+      </div>`;
       cartDisplay.appendChild(newItem);
       itemToAdd.quantity--;
     }
